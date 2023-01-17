3e6eb1d23fad0740224c24801d5ad5f4a30a26d0
Naman Sharma <76205377+naman2108@users.noreply.github.com>
2023-01-17 21:34:04 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+while True:
+        notification.notify(
+            title='Please Drink Water Now!',
+            message='If you don’t stay hydrated, your physical performance can suffer.',
+            app_icon="D:\water drinking reminder project\icon.ico.ico",
+            timeout=2
+    )
+# print('naman
+        time.sleep(6)
+    
